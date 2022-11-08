e94777a2bbe2536a017447558346c98e2e91f7dc
JaimeAlberto <jaime.alberto.vicente@gmail.com>
2022-11-08 16:57:58 +0100

    up

--- Linux/zerotier.sh
+++ Linux/zerotier.sh
@@ -7,6 +7,8 @@ NETWORK="<ID_NETWORK_ZEROTIER>"
 # Descarga zerotier
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | bash; fi
+systemctl enable zerotier-one
+systemctl start zerotier-one
 zerotier-cli join $NETWORK
 
 # Borrar script despues de su ejecucion deshabilitado
